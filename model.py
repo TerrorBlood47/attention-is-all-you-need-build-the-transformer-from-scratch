9a7abc25e5779403e0ea62f44133d2327385e887
Ahaj Mahhin Faiak <115548254+TerrorBlood47@users.noreply.github.com>
2026-07-19 02:13:17 +0600

    Step 41: assemble_encoder_layer

--- model.py
+++ model.py
@@ -364,8 +364,22 @@ def encoder_layer_feed_forward_sublayer(x, w1, b1, w2, b2, gamma, beta):
     y = apply_residual_add_and_norm(x, pos_ffn_out, gamma, beta)
     return y
 
-# Step 41 - assemble_encoder_layer (not yet solved)
-# TODO: implement
+# Step 41 - assemble_encoder_layer
+def assemble_encoder_layer(x, layer_params, num_heads, src_mask):
+    # TODO: chain the self-attention sublayer and the feed-forward sublayer using layer_params.
+    w_q, w_k, w_v, w_o, attn_gamma, attn_beta, w1, b1, w2, b2, ffn_gamma, ffn_beta = \
+    layer_params["w_q"], layer_params["w_k"], layer_params["w_v"], layer_params["w_o"], \
+    layer_params["attn_gamma"], layer_params["attn_beta"], \
+    layer_params["w1"], layer_params["b1"], layer_params["w2"], layer_params["b2"], \
+    layer_params["ffn_gamma"], layer_params["ffn_beta"]
+    
+    multi_head_attn_sublayer_output = encoder_layer_self_attention_sublayer(x, w_q, \
+    w_k, w_v, w_o, attn_gamma, attn_beta, num_heads, src_mask)
+    
+    ffn_output = encoder_layer_feed_forward_sublayer(multi_head_attn_sublayer_output, \
+    w1, b1, w2, b2, ffn_gamma, ffn_beta)
+
+    return ffn_output
 
 # Step 42 - stack_encoder_layers (not yet solved)
 # TODO: implement
